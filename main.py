cb7e29cfb7022ab5f818fb56e32d92a1637e285a
AyushPatel884 <111659589+AyushPatel884@users.noreply.github.com>
2022-08-20 17:23:46 -0400

    learning_rate = 1e-02

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import time
 # hyperparameters
 H = 200  # number of hidden layer neurons
 batch_size = 10  # every how many episodes to do a param update?
-learning_rate = 1e-4
+learning_rate = 1e-2
 gamma = 0.99  # discount factor for reward
 decay_rate = 0.99  # decay factor for RMSProp leaky sum of grad^2
 resume = False  # resume from previous checkpoint?
@@ -136,6 +136,7 @@ while True:
             TimeDiff = end - start
             Min, Sec + divmod(TimeDiff, 60)
             Hour, Min = divmod(Min, 60)
+            print("this task is done by Ayuesh patel C0855884)
             print("Hours:", Hour)
             print("Min", Min)
             print("Sec", Sec)
